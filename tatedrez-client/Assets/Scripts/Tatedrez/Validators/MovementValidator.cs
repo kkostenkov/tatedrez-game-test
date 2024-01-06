51c5a246a39f10e3911aa8e29d3249cc6e48e900
kkostenkov <kkostenkov@gmail.com>
2024-01-06 07:25:55 +0100

    Implement rook movement validation

--- tatedrez-client/Assets/Scripts/Tatedrez/Validators/MovementValidator.cs
+++ tatedrez-client/Assets/Scripts/Tatedrez/Validators/MovementValidator.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Tatedrez.Models;
 using Tatedrez.ModelServices;
 
@@ -35,17 +37,39 @@ namespace Tatedrez
         {
             var from = move.From;
             var to = move.To;
-            var isTheSameRow = from.X == to.X;
-            var isTheSameColumn = from.Y == to.Y;
+            var isTheSameRow = from.Y == to.Y;
+            var isTheSameColumn = from.X == to.X;
             var isStraightLine = isTheSameRow || isTheSameColumn;
             if (!isStraightLine) {
                 return false;
             }
-
+            // Does jump over pieces?
             if (isTheSameRow) {
-                Enumerable.Range(1, 10);
-                board.PeekPiece()
+                var min = Math.Min(from.X, to.X);
+                var max = Math.Max(from.X, to.X);
+                var range = Enumerable.Range(min, max);
+                foreach (var number in range) {
+                    var coords = new BoardCoords(number, from.X);
+                    var piece = board.PeekPiece(coords);
+                    if (piece != null && piece.Guid != move.PieceGuid) {
+                        return false;
+                    }
+                }
             }
+            else if (isTheSameColumn) {
+                var min = Math.Min(from.Y, to.Y);
+                var max = Math.Max(from.Y, to.Y);
+                var range = Enumerable.Range(min, max);
+                foreach (var number in range) {
+                    var coords = new BoardCoords(from.X, number);
+                    var piece = board.PeekPiece(coords);
+                    if (piece != null && piece.Guid != move.PieceGuid) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
--- tatedrez-client/Assets/Tests/Validators/MovementValidatorTest.Rook.cs
+++ tatedrez-client/Assets/Tests/Validators/MovementValidatorTest.Rook.cs
@@ -14,6 +14,7 @@ namespace MovementValidatorTests
                 yield return new TestCaseData(new BoardCoords(1, 1), new BoardCoords(1, 2)).SetName("1:1 -> 1:2");
                 yield return new TestCaseData(new BoardCoords(1, 1), new BoardCoords(0, 1)).SetName("1:1 -> 0:1");
                 yield return new TestCaseData(new BoardCoords(1, 1), new BoardCoords(2, 1)).SetName("1:1 -> 2:1");
+                yield return new TestCaseData(new BoardCoords(2, 0), new BoardCoords(2, 2)).SetName("2:0 -> 2:2");
             }
         }
 
@@ -70,6 +71,8 @@ namespace MovementValidatorTests
                     .SetName("0:0 over 0:1 to 0:2");
                 yield return new TestCaseData(new BoardCoords(1, 2), new BoardCoords(1, 1), new BoardCoords(1, 0))
                     .SetName("1:2 over 1:1 to 1:0");
+                yield return new TestCaseData(new BoardCoords(0, 2), new BoardCoords(1, 2), new BoardCoords(2, 0))
+                    .SetName("0:2 over 1:2 to 2:0");
             }
         }
 
