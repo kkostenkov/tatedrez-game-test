773c19f6b3508eb0f536c6dd3e4bae1529e68709
kkostenkov <kkostenkov@gmail.com>
2024-01-07 02:36:43 +0100

    Delete GameSessionFlow.cs

--- tatedrez-client/Assets/Scripts/Views/SessionCoordinator.cs
+++ tatedrez-client/Assets/Scripts/Views/SessionCoordinator.cs
@@ -1,33 +1,37 @@
+using System.Runtime.ConstrainedExecution;
+using System.Threading.Tasks;
+using Tatedrez.Models;
 using Tatedrez.Views;
 using UnityEngine;
 
 namespace Tatedrez
 {
-   public class SessionCoordinator : MonoBehaviour
-   {
-      [SerializeField]
-      private GameSessionView sessionView;
+    public class SessionCoordinator : MonoBehaviour
+    {
+        [SerializeField]
+        private GameSessionView sessionView;
 
-      private GameSessionRepository sessionRepo = new GameSessionRepository();
-      private GameSessionFlow flow;
+        private GameSessionRepository sessionRepo = new GameSessionRepository();
+        private GameSessionController gameSessionController;
 
-      private async void Awake()
-      {
-         flow = new GameSessionFlow();
-         var data = this.sessionRepo.Load();
-         var inputManager = new PlayerInputManager();
-         this.sessionView.BindLocalInputForPlayer(0, inputManager);
-         this.sessionView.BindLocalInputForPlayer(1, inputManager);
-         
-         await flow.Prepare(data, this.sessionView, inputManager, inputManager);
-      }
+        private async void Awake()
+        {
+            var data = this.sessionRepo.Load();
+            var inputManager = new PlayerInputManager();
+            this.sessionView.BindLocalInputForPlayer(0, inputManager);
+            this.sessionView.BindLocalInputForPlayer(1, inputManager);
 
-      private async void Start()
-      {
-         while (flow.IsRunning) {
-            await this.flow.ProcessTurn();
-         }
-         await this.flow.ProcessTurn();
-      }
-   }
+            this.gameSessionController = new GameSessionController(data, sessionView, inputManager, inputManager);
+            await this.gameSessionController.BuildBoardAsync();
+        }
+
+        private async void Start()
+        {
+            while (this.gameSessionController.IsSessionRunning) {
+                await this.gameSessionController.Turn();
+            }
+
+            await this.gameSessionController.Turn();
+        }
+    }
 }
