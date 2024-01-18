d62bf501a9831fb577f03624aaae1160df91e3bb
kkostenkov <kkostenkov@gmail.com>
2024-01-18 06:52:22 +0100

    reactor tictactoe validation

--- tatedrez-client/Assets/Scripts/Tatedrez/Validators/BoardValidator.cs
+++ tatedrez-client/Assets/Scripts/Tatedrez/Validators/BoardValidator.cs
@@ -14,117 +14,141 @@ namespace Tatedrez
         private readonly List<BoardCoords> diagonalTwo = new() {
             new BoardCoords(0, 2), new BoardCoords(1, 1), new BoardCoords(2, 0)
         };
-
+        
+        private delegate bool TicTacToeChecker(IBoardInfoService board, ref List<BoardCoords> winSequence);
+        
         public bool TryFindTickTackToe(IBoardInfoService board, out EndGameDetails endGameDetails)
         {
-            endGameDetails = HasVerticalTicTacToe(board);
-            if (endGameDetails != null) {
+            List<TicTacToeChecker> checks = new List<TicTacToeChecker>() {
+                HasVerticalTicTacToe,
+                HasHorizontalTicTacToe,
+                HasDiagonalTicTacToe
+            };
+            
+            var winningCoords = new List<BoardCoords>();
+            var hasTicTacToe = checks.Any(check => check.Invoke(board, ref winningCoords));
+            if (hasTicTacToe) {
+                endGameDetails = ComposeEndgameDetails(board, winningCoords);
                 return true;
             }
-
-            endGameDetails = HasHorizontalTicTacToe(board);
-            if (endGameDetails != null) {
-                return true;
-            }
-            endGameDetails = HasDiagonalTicTacToe(board);
-            return endGameDetails != null;
+            
+            endGameDetails = null;
+            return false;
         }
 
-        private EndGameDetails HasVerticalTicTacToe(IBoardInfoService board)
+        private static EndGameDetails ComposeEndgameDetails(IBoardInfoService board, List<BoardCoords> winningCoords)
         {
-            var details = new EndGameDetails();
+            var firstPiece = board.PeekPiece(winningCoords[0]);
+            var endGameDetails = new EndGameDetails() {
+                WinnerId = firstPiece.Owner,
+            };
+            for (var index = 0; index < winningCoords.Count; index++) {
+                endGameDetails.WinnerCords[index] = winningCoords[index];
+            }
+
+            return endGameDetails;
+        }
+
+        private static bool HasVerticalTicTacToe(IBoardInfoService board, ref List<BoardCoords> winningCoords)
+        {
+            winningCoords.Clear();
             var size = board.GetSize();
             for (int x = 0; x < size.X; x++) {
                 var coords = new BoardCoords(x, 0);
                 var firstPiece = board.PeekPiece(coords);
                 if (firstPiece == null) {
-                    details.Clear();
+                    winningCoords.Clear();
                     continue;
                 }
                 var firstPieceOwner = board.PeekPiece(coords).Owner;
                 
-                details.WinnerCords[0] = coords;
-                details.WinnerId = firstPieceOwner;
+                winningCoords.Add(coords);
                 
                 for (int y = 1; y < size.Y; y++) {
                     coords = new BoardCoords(x, y);
                     var piece = board.PeekPiece(coords);
                     if (piece == null || firstPieceOwner != piece.Owner) {
-                        details.Clear();
+                        winningCoords.Clear();
                         break;
                     }
-                    details.WinnerCords[y] = coords;
+                    winningCoords.Add(coords);
                 }
-                if (details.HasWinner) {
+                if (winningCoords.Count == board.GetSize().Y) {
                     break;
                 }
             }
 
-            return details.HasWinner ? details : null;
+            var boardSide = board.GetSize().Y;
+            return winningCoords.Count == boardSide && winningCoords.Count != 0;
         }
 
-        private EndGameDetails HasHorizontalTicTacToe(IBoardInfoService board)
+        private static bool HasHorizontalTicTacToe(IBoardInfoService board, ref List<BoardCoords> winningCoords)
         {
-            var details = new EndGameDetails();
+            winningCoords.Clear();
             var size = board.GetSize();
             for (int y = 0; y < size.Y; y++) {
                 var coords = new BoardCoords(0, y);
                 var firstPiece = board.PeekPiece(coords);
                 if (firstPiece == null) {
-                    details.Clear();
+                    winningCoords.Clear();
                     continue;
                 }
                 var firstPieceOwner = firstPiece.Owner;
                 
-                details.WinnerCords[0] = coords;
-                details.WinnerId = firstPieceOwner;
+                winningCoords.Add(coords);
                 
                 for (int x = 1; x < size.X; x++) {
                     coords = new BoardCoords(x, y);
                     var piece = board.PeekPiece(coords);
                     if (piece == null || firstPieceOwner != piece.Owner) {
-                        details.Clear();
+                        winningCoords.Clear();
                         break;
                     }
-                    details.WinnerCords[x] = coords;
+                    winningCoords.Add(coords);
                 }
 
-                if (details.HasWinner) {
+                if (winningCoords.Count == board.GetSize().X) {
                     break;
                 }
             }
 
-            return details.HasWinner ? details : null;
+            return winningCoords.Count == board.GetSize().X && winningCoords.Count != 0;
         }
 
-        private EndGameDetails HasDiagonalTicTacToe(IBoardInfoService board)
+        private bool HasDiagonalTicTacToe(IBoardInfoService board, ref List<BoardCoords> winningCoords)
         {
-            var firstDiagonal = this.diagonalOne.All(coord =>
-                board.PeekPiece(coord) != null
-                && (board.PeekPiece(coord).Owner == board.PeekPiece(this.diagonalOne[0]).Owner));
-            if (firstDiagonal) {
-                return CreateDiagonalWinEndGameDetails(board, this.diagonalOne);
-            }
-            var secondDiagonal = this.diagonalTwo.All(coord =>
-                board.PeekPiece(coord) != null
-                && (board.PeekPiece(coord)?.Owner == board.PeekPiece(this.diagonalTwo[0])?.Owner));
-            if (secondDiagonal) {
-                return CreateDiagonalWinEndGameDetails(board, this.diagonalTwo);
+            winningCoords.Clear();
+            
+            var diagonals = board.Diagonals;
+            if (diagonals == null) {
+                return false;
             }
 
-            return null;
+            foreach (var diagonal in diagonals) {
+                if (CheckDiagonal(diagonal, board, out winningCoords)) {
+                    return true;
+                }    
+            }
+            
+            return false;
         }
 
-        private EndGameDetails CreateDiagonalWinEndGameDetails(IBoardInfoService board, List<BoardCoords> diagonal)
+        private bool CheckDiagonal(List<BoardCoords> diagonal, IBoardInfoService board, out List<BoardCoords> winningCoords)
         {
-            var endGameDetails = new EndGameDetails() {
-                WinnerId = board.PeekPiece(diagonal[0]).Owner,
-            };
-            for (int i = 0; i < diagonal.Count; i++) {
-                endGameDetails.WinnerCords[i] = diagonal[i];
+            winningCoords = null;
+            if (diagonal == null || diagonal.Count == 0) {
+                return false;
+            }
+            
+            var hasTicTacToe = diagonal.All(coord =>
+                board.PeekPiece(coord) != null
+                && (board.PeekPiece(coord).Owner == board.PeekPiece(diagonal[0]).Owner));
+            if (!hasTicTacToe) {
+                return false;
             }
 
-            return endGameDetails;
+            winningCoords = new List<BoardCoords>(diagonal);
+            return true;
         }
 
         public bool IsValidMove(IBoardInfoService board, PlacementMove move)
